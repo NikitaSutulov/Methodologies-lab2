ba0876dcfda013f4806b2ab781241ec068051e4f
Nikita Sutulov <notsid212@gmail.com>
2023-03-18 14:47:52 +0200

    added comments
    Added comments for the future methods of DoublyLinkedList class for the possible reader to understand their purpose more easily.

--- src/doublyLinkedList.js
+++ src/doublyLinkedList.js
@@ -9,50 +9,72 @@ class DoublyLinkedList {
 
     // TODO: implement methods
 
+    // length method returns the count of elements in the list
+    // if the list is empty, it returns zero
     length() {
 
     }
 
+    // append method adds an element at the end of the list
     append(element) {
 
     }
 
+    // insert method inserts an element in a certain position of the list
+    // if index is less than 0 or larger than the list range,
+    // it will throw an error
     insert(element, index) {
 
     }
 
+    // delete method deletes an element in a certain position of the list and returns its value
+    // if index is less than 0 or larger than the list range,
+    // it will throw an error
     delete(index) {
 
     }
 
+    // deleteAll method deletes all the elements in the list with a certain value
+    // if there are no such elements, nothing changes in the list
     deleteAll(element) {
 
     }
 
+    // get method returns an element of the list in a certain position
+    // if index is less than 0 or larger than the list range,
+    // it will throw an error
     get(index) {
 
     }
 
+    // clone methods copies elements from the list and returns a copy of it
     clone() {
 
     }
 
+    // reverse method reverses the list
     reverse() {
 
     }
 
+    // findFirst method finds the first element of the list with a certain value and returns its index
+    // if there are no such elements, it returns -1
     findFirst(element) {
 
     }
 
+    // findLast method finds the last element of the list with a certain value and returns its index
+    // if there are no such elements, it returns -1
     findLast(element) {
 
     }
 
+    // clear method deletes the elements from the list
     clear() {
 
     }
 
+    // extend method adds to the list all the elements of elements list
     extend(elements) {
 
     }
