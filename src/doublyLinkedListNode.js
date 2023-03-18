5262a432d4c3ff8f4976576daff986889d184562
Nikita Sutulov <notsid212@gmail.com>
2023-03-18 14:07:23 +0200

    implemented doubly linked list node
    Implemented a class for the doubly linked list node in the file src/doublyLinkedListNode.js

---
+++ src/doublyLinkedListNode.js
@@ -0,0 +1,14 @@
+'use strict';
+class DoublyLinkedListNode {
+    constructor (val, prev  = null, next = null) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    toString() {
+        return `${this.val}`;
+    }
+}
+
+export { DoublyLinkedListNode };
