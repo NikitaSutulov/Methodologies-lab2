07d30780a83f80d4215d2c7354e869f06472bd8a
Nikita Sutulov <notsid212@gmail.com>
2023-03-18 14:25:48 +0200

    implemented DoublyLinkedList class structure
    Created class DoblyLinkedList with a constructor and methods not implemented yet: length, append, insert, delete, deleteAll, get, clone, reverse, findFirst, findLast, clear, extend.

---
+++ src/doublyLinkedList.js
@@ -0,0 +1,59 @@
+'use strict';
+// import { DoublyLinkedListNode } from './doublyLinkedListNode.js';
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // TODO: implement methods
+
+    length() {
+
+    }
+
+    append(element) {
+
+    }
+
+    insert(element, index) {
+
+    }
+
+    delete(index) {
+
+    }
+
+    deleteAll(element) {
+
+    }
+
+    get(index) {
+
+    }
+
+    clone() {
+
+    }
+
+    reverse() {
+
+    }
+
+    findFirst(element) {
+
+    }
+
+    findLast(element) {
+
+    }
+
+    clear() {
+
+    }
+
+    extend(elements) {
+
+    }
+}
--- src/doublyLinkedListNode.js
+++ src/doublyLinkedListNode.js
@@ -1,6 +1,6 @@
 'use strict';
 class DoublyLinkedListNode {
-    constructor (val, prev  = null, next = null) {
+    constructor(val, prev  = null, next = null) {
         this.val = val;
         this.prev = prev;
         this.next = next;
